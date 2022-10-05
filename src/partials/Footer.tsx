bf04ede27d4b67d4e931dc2ac46291699f529431
Yog Sharma <yogthesharma@gmail.com>
2022-10-05 22:13:12 +0530

    feat: removed spelling mistakes

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => (
   <Section>
     <div className="relative flex flex-col items-center py-5">
       <div className="w-full grow border-t border-gray-700"></div>
-      <p className="mt-4 font-semibold">Built with ❤️ in India</p>
+      <p className="mt-4 font-semibold">Made with ❤️ in India</p>
     </div>
   </Section>
 );
